26a72219b737f9395697ee9f3ee724fe27dd4daf
Nick Correa Garcia <nickcorreagarcia@gmail.com>
2025-07-31 12:44:38 -0500

    Setting styles globals

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
+import "./index.css";
+import "./styles/base.scss";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "./index.css";
 import { UserProvider } from "./contexts/UserContext";
+import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -12,5 +13,5 @@ createRoot(document.getElementById("root")!).render(
         <App />
       </UserProvider>
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 );
