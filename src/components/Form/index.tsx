de261c3952d4f7d8ac77f51d64030a8be5b351fb
Nick Correa Garcia <nickcorreagarcia@gmail.com>
2025-07-31 20:14:30 -0500

    Update styles and code in index for form

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,14 +1,45 @@
 import { useUser } from "../../contexts/UserContext";
 import UserForm from "../../components/Form/UserForm";
 import styles from "./Form.module.scss";
+import bgLightBlueDesktop from "../../assets/blur-asset-lightblue-desktop.png";
+import bgPurpleDesktop from "../../assets/blur-asset-purple-desktop.png";
+import bgLightBlueMobile from "../../assets/blur-asset-lightblue-mobile.png";
+import bgPurpleMobile from "../../assets/blur-asset-purple-mobile.png";
+import maskGroupMobile from "../../assets/maskGroup-mobile.png";
+import maskGroupDesktop from "../../assets/maskGroup-desktop.png";
+import { useMediaQuery } from "@mui/material";
 
 const FormPage = () => {
   const { setUser } = useUser();
+  const isMobile = useMediaQuery("(max-width:768px)");
+
+  const backgroundStyle = {
+    backgroundImage: isMobile
+      ? `url(${bgLightBlueMobile}), url(${bgPurpleMobile})`
+      : `url(${bgLightBlueDesktop}), url(${bgPurpleDesktop})`,
+    backgroundRepeat: "no-repeat, no-repeat",
+    backgroundPosition: "top right, bottom left",
+    backgroundSize: "contain, contain",
+  };
 
   return (
-    <div className={styles.container}>
-      <h1>Creado para ti y tu familia</h1>
-      <UserForm onUserLoaded={setUser} />
+    <div className={styles.container} style={backgroundStyle}>
+      {isMobile ? null : <img src={maskGroupDesktop} alt="Mask Group" />}
+      <div className={styles.container__content}>
+        <div className={styles.container__contentTop}>
+          <div className={styles.container__contentTop__div}>
+            <div className={styles.container__contentTop__span}>
+              Seguro Salud Flexible
+            </div>
+            <h1 className={styles.container__contentTop__h1}>
+              Creado para ti y tu familia
+            </h1>
+          </div>
+          {isMobile ? <img src={maskGroupMobile} alt="Mask Group" /> : null}
+        </div>
+        <hr className={styles.container__line} />
+        <UserForm onUserLoaded={setUser} />
+      </div>
     </div>
   );
 };
