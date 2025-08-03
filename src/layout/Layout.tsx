6abb8c0847dd98018ce84982a6dea48708543a4e
Nick Correa Garcia <nickcorreagarcia@gmail.com>
2025-08-02 22:37:08 -0500

    Add Footer component in Layout

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,5 +1,6 @@
 import type { FC, ReactNode } from "react";
 import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
 
 interface Props {
   children: ReactNode;
@@ -10,7 +11,7 @@ const Layout: FC<Props> = ({ children }) => {
     <>
       <Header />
       <main className="containerMain">{children}</main>
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 };
