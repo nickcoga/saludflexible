779d900aa87757b60597330694360eac9a9ad252
Nick Correa Garcia <nickcorreagarcia@gmail.com>
2025-08-03 17:33:43 -0500

    ID correction for gradients in SVG

--- src/icons/addUser.tsx
+++ src/icons/addUser.tsx
@@ -17,18 +17,19 @@ const IconAddUser: FC<Props> = ({
     height={height}
     className={className}
     fill="none"
+    viewBox="0 0 36 36"
   >
     <path
-      fill="url(#a)"
+      fill="url(#addUserGradientA)"
       d="M28.819 7.216 16.956 1.285a2.153 2.153 0 0 0-1.912 0L3.18 7.216a2.145 2.145 0 0 0-1.14 2.331l1.661 8.304a13.991 13.991 0 0 0 5.423 8.524l5.61 4.144a2.125 2.125 0 0 0 2.54 0l.709-.524c-.472-.473-.886-1-1.234-1.57l-.663.489a.135.135 0 0 1-.162 0l-5.61-4.144c-.45-.332-.876-.695-1.276-1.086a7.067 7.067 0 0 1 3.553-5.027L14 17.875a5 5 0 0 0 7-4.584V11.71a5 5 0 0 0-10 0v1.582a4.98 4.98 0 0 0 1.23 3.279l-.605.336a9.068 9.068 0 0 0-4.154 4.887A12 12 0 0 1 5.67 17.46l-1.67-8.305a.14.14 0 0 1 .075-.15l11.862-5.931a.144.144 0 0 1 .125 0L27.924 9a.14.14 0 0 1 .073.15l-1.353 6.765a8.397 8.397 0 0 1 1.865.875l1.45-7.25a2.146 2.146 0 0 0-1.14-2.324Zm-15.82 4.493a3 3 0 0 1 6 0v1.582a3 3 0 1 1-6 0V11.71Z"
     />
     <path
-      fill="url(#b)"
+      fill="url(#addUserGradientB)"
       d="M28.95 19.05a7 7 0 1 0-9.899 9.9 7 7 0 0 0 9.899-9.9ZM28 25h-3v3h-2v-3h-3v-2h3v-3h2v3h3v2Z"
     />
     <defs>
       <linearGradient
-        id="a"
+        id="addUserGradientA"
         x1={2}
         x2={18.712}
         y1={1.061}
@@ -39,7 +40,7 @@ const IconAddUser: FC<Props> = ({
         <stop offset={0.658} stopColor="#13172C" />
       </linearGradient>
       <linearGradient
-        id="b"
+        id="addUserGradientB"
         x1={17.367}
         x2={28.902}
         y1={18.099}
@@ -52,4 +53,5 @@ const IconAddUser: FC<Props> = ({
     </defs>
   </svg>
 );
+
 export default IconAddUser;
--- src/icons/protection.tsx
+++ src/icons/protection.tsx
@@ -17,18 +17,19 @@ const IconProtection: FC<Props> = ({
     height={height}
     className={className}
     fill="none"
+    viewBox="0 0 36 36"
   >
     <path
-      fill="url(#a)"
+      fill="url(#protectionGradientA)"
       d="M29.959 9.547a2.145 2.145 0 0 0-1.14-2.33L16.956 1.284a2.153 2.153 0 0 0-1.912 0L3.18 7.216a2.145 2.145 0 0 0-1.14 2.331l1.661 8.304a13.991 13.991 0 0 0 5.423 8.524l.514.375c.03-.755.152-1.503.362-2.229a11.992 11.992 0 0 1-4.332-7.069l-1.67-8.297a.14.14 0 0 1 .075-.15l11.862-5.931a.144.144 0 0 1 .125 0L27.924 9a.14.14 0 0 1 .073.15l-1.66 8.305a12.025 12.025 0 0 1-.943 2.824c.529.493.999 1.046 1.4 1.647a13.999 13.999 0 0 0 1.5-4.08l1.665-8.299Z"
     />
     <path
-      fill="url(#b)"
+      fill="url(#protectionGradientB)"
       d="m22.866 20.135-1.953-1.079A4 4 0 0 0 23 15.541V13.96a4 4 0 0 0-8 0v1.582a4 4 0 0 0 2.087 3.515l-1.953 1.079a8 8 0 0 0-4.134 7V29a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-1.861a8 8 0 0 0-4.134-7.004Z"
     />
     <defs>
       <linearGradient
-        id="a"
+        id="protectionGradientA"
         x1={2}
         x2={14.974}
         y1={1.061}
@@ -39,7 +40,7 @@ const IconProtection: FC<Props> = ({
         <stop offset={0.658} stopColor="#13172C" />
       </linearGradient>
       <linearGradient
-        id="b"
+        id="protectionGradientB"
         x1={11.419}
         x2={27.521}
         y1={11.532}
@@ -52,4 +53,5 @@ const IconProtection: FC<Props> = ({
     </defs>
   </svg>
 );
+
 export default IconProtection;
