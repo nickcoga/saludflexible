cb28c99435fa246b107074ec76f77c72c5269fb1
Nick Correa Garcia <nickcorreagarcia@gmail.com>
2025-08-03 16:23:51 -0500

    refactor code in Plans page with new styles and components

--- src/pages/Plans/index.tsx
+++ src/pages/Plans/index.tsx
@@ -5,11 +5,13 @@ import type { Plan } from "../../types/plan";
 import { useUser } from "../../contexts/UserContext";
 import { getAgeFromBirthdate } from "../../utils/getAge";
 import styles from "./Plans.module.scss";
+import IconProtection from "../../icons/protection";
+import IconAddUser from "../../icons/addUser";
+import Button from "../../components/Button/Button";
 
 const PlansPage = () => {
   const { user } = useUser();
   const navigate = useNavigate();
-
   const [plans, setPlans] = useState<Plan[]>([]);
   const [target, setTarget] = useState<"me" | "someone-else">("me");
 
@@ -51,33 +53,48 @@ const PlansPage = () => {
 
   return (
     <div className={styles.plans}>
-      <h2 className={styles.plans__title}>
-        {user?.name} ¿Para quién deseas cotizar?
-      </h2>
+      <div className={styles.plans__contentTop}>
+        <h2 className={styles.plans__title}>
+          {user?.name} ¿Para quién deseas cotizar?
+        </h2>
+        <p className={styles.plans__subtitle}>
+          Selecciona la opción que se ajuste más a tus necesidades.
+        </p>
+      </div>
 
       <div className={styles.plans__toggle}>
-        <button
-          className={`${styles.plans__toggle_button} ${target === "me" ? styles["plans__toggle_button--active"] : ""}`}
-          onClick={() => setTarget("me")}
-        >
-          Para mí
-        </button>
-        <button
-          className={`${styles.plans__toggle_button} ${target === "someone-else" ? styles["plans__toggle_button--active"] : ""}`}
-          onClick={() => setTarget("someone-else")}
-        >
-          Para alguien más
-        </button>
+        <div className={styles.plans__content}>
+          <div className={styles.plans__content__top}>
+            <IconProtection />
+            <button
+              className={`${styles.plans__toggle_button} ${target === "me" ? styles["plans__toggle_button--active"] : ""}`}
+              onClick={() => setTarget("me")}
+            >
+              Para mí
+            </button>
+          </div>
+          <p className={styles.plans__toggle__text}>
+            Cotiza tu seguro de salud y agrega familiares si así lo deseas.
+          </p>
+        </div>
+        <div className={styles.plans__content}>
+          <div className={styles.plans__content__top}>
+            <IconAddUser />
+            <button
+              className={`${styles.plans__toggle_button} ${target === "someone-else" ? styles["plans__toggle_button--active"] : ""}`}
+              onClick={() => setTarget("someone-else")}
+            >
+              Para alguien más
+            </button>
+          </div>
+          <p>Cotiza tu seguro de salud y agrega familiares si así lo deseas.</p>
+        </div>
       </div>
 
       {plans.length > 0 && (
         <div className={styles.plans__list}>
           {plans.map((plan: Plan) => (
-            <div
-              key={plan.name}
-              className={styles.plans__card}
-              onClick={() => handleSelectPlan(plan)}
-            >
+            <div key={plan.name} className={styles.plans__card}>
               <h3 className={styles.plans__card_title}>{plan.name}</h3>
               <p className={styles.plans__card_price}>S/ {getPrice(plan)}</p>
               <ul className={styles.plans__card_benefits}>
@@ -85,6 +102,13 @@ const PlansPage = () => {
                   <li key={idx}>{d}</li>
                 ))}
               </ul>
+              <Button
+                type="button"
+                variant="primary"
+                onClick={() => handleSelectPlan(plan)}
+              >
+                Seleccionar Plan
+              </Button>
             </div>
           ))}
         </div>
