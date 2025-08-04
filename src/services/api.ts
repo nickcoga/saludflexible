02b7319d4f3166ed7f797f7383b0c343e50ee81e
Nick Correa Garcia <nickcorreagarcia@gmail.com>
2025-08-04 02:20:09 -0500

    Corrige ruta fetch para compatibilidad con iOS

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,24 +5,18 @@ interface PlansResponse {
   list: Plan[];
 }
 
+const base = import.meta.env.BASE_URL;
+
 export const fetchUser = async (): Promise<User> => {
   try {
     const res = await fetch(
       "https://rimac-front-end-challenge.netlify.app/api/user.json"
     );
-    if (!res.ok) throw new Error("Error desde el API remoto");
-
-    return await res.json();
+    if (!res.ok) throw new Error();
+    return res.json();
   } catch (error) {
-    console.warn("Fallo el API remoto (user), usando backup local:", error);
-
-    const localRes = await fetch(`${import.meta.env.BASE_URL}data/user.json`);
-    if (!localRes.ok)
-      throw new Error(
-        "No se pudo cargar ni remotamente ni localmente el usuario"
-      );
-
-    return await localRes.json();
+    const fallbackRes = await fetch(`${base}data/user.json`);
+    return fallbackRes.json();
   }
 };
 
@@ -31,20 +25,11 @@ export const fetchPlans = async (): Promise<Plan[]> => {
     const res = await fetch(
       "https://rimac-front-end-challenge.netlify.app/api/plans.json"
     );
-    if (!res.ok) throw new Error("Error desde el API remoto");
-
     const data: PlansResponse = await res.json();
     return data.list;
   } catch (error) {
-    console.warn("Fallo el API remoto (plans), usando backup local:", error);
-
-    const localRes = await fetch(`${import.meta.env.BASE_URL}data/plans.json`);
-    if (!localRes.ok)
-      throw new Error(
-        "No se pudo cargar ni remotamente ni localmente los planes"
-      );
-
-    const localData: PlansResponse = await localRes.json();
-    return localData.list;
+    const fallbackRes = await fetch(`${base}data/plans.json`);
+    const data: PlansResponse = await fallbackRes.json();
+    return data.list;
   }
 };
